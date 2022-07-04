704d22a25556d13dd3575f98f2396764ca0d6004
ekonovalov <konovalov.ye@gmail.com>
2022-07-04 18:59:51 +0400

    building structs

--- note_app/src/main.rs
+++ note_app/src/main.rs
@@ -1,14 +1,43 @@
-fn alter_number(number: &mut String) {
-    number.push("!".chars().next().unwrap());
+use std::collections::HashMap;
+
+enum AllowedData {
+    S(String),
+    I(i8),
 }
 
-fn print_number(number: &String) {
-    println!("{}", number);
+struct CustomMap {
+    body: HashMap<String, AllowedData>,
+}
+
+impl CustomMap {
+    fn new() -> CustomMap {
+        return CustomMap {
+            body: HashMap::new(),
+        };
+    }
+    fn get(&self, key: &str) -> &AllowedData {
+        return self.body.get(key).unwrap();
+    }
+    fn insert(&mut self, key: &str, value: AllowedData) -> () {
+        self.body.insert(key.to_string(), value);
+    }
+    fn display(&self, key: &str) -> () {
+        match self.get(key) {
+            AllowedData::I(value) => println!("{}", value),
+            AllowedData::S(value) => println!("{}", value),
+        }
+    }
 }
 
 fn main() {
-    let mut one: String = String::from("one");
-    print_number(&one);
-    alter_number(&mut one);
-    println!("{}", one);
+    // defining a new hash map
+    let mut map = CustomMap::new();
+
+    // inserting two different types of data
+    map.insert("test", AllowedData::I(8));
+    map.insert("testing", AllowedData::S("test value".to_string()));
+
+    // displaying the date
+    map.display("test");
+    map.display("testing");
 }
--- note_app/target/debug/note_app.d
+++ note_app/target/debug/note_app.d
@@ -1,1 +1,1 @@
-/Users/macos/Dev/rust_playaround/note_app/target/debug/note_app: /Users/macos/Dev/rust_playaround/note_app/src/main.rs
+/home/ekonovalov/projects/rust_playaround/note_app/target/debug/note_app: /home/ekonovalov/projects/rust_playaround/note_app/src/main.rs
