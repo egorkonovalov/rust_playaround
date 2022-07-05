0df0f570dbd8a01d5d51240f07cf8940f52c7559
Egor Konovalov <73017521+egorkonovalov@users.noreply.github.com>
2022-07-05 23:27:32 +0400

    Structuring code

--- note_app/src/main.rs
+++ note_app/src/main.rs
@@ -1,43 +1,16 @@
-use std::collections::HashMap;
+mod to_do;
 
-enum AllowedData {
-    S(String),
-    I(i8),
-}
-
-struct CustomMap {
-    body: HashMap<String, AllowedData>,
-}
-
-impl CustomMap {
-    fn new() -> CustomMap {
-        return CustomMap {
-            body: HashMap::new(),
-        };
-    }
-    fn get(&self, key: &str) -> &AllowedData {
-        return self.body.get(key).unwrap();
-    }
-    fn insert(&mut self, key: &str, value: AllowedData) -> () {
-        self.body.insert(key.to_string(), value);
-    }
-    fn display(&self, key: &str) -> () {
-        match self.get(key) {
-            AllowedData::I(value) => println!("{}", value),
-            AllowedData::S(value) => println!("{}", value),
-        }
-    }
-}
+use to_do::structs::traits::create::Create;
+use to_do::to_do_factory;
+use to_do::ItemTypes;
 
 fn main() {
-    // defining a new hash map
-    let mut map = CustomMap::new();
-
-    // inserting two different types of data
-    map.insert("test", AllowedData::I(8));
-    map.insert("testing", AllowedData::S("test value".to_string()));
-
-    // displaying the date
-    map.display("test");
-    map.display("testing");
+    let to_do_item: Result<ItemTypes, &'static str> = to_do_factory("done", "washing");
+    match to_do_item.unwrap() {
+        ItemTypes::Pending(item) => item.create(&item.super_struct.title),
+        ItemTypes::Done(item) => println!(
+            "it's a done item with the title: {}",
+            item.super_struct.title
+        ),
+    }
 }
